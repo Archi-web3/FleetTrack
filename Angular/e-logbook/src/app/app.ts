9e258fec42cbe0344d7662cf4c7c479728ce0e19
Jonathan <jonathan.genet@gmail.com>
2026-01-23 15:09:05 +0100

    feat(e-logbook): implement offline architecture with auto-sync and login warning

--- Angular/e-logbook/src/app/app.ts
+++ Angular/e-logbook/src/app/app.ts
@@ -1,12 +1,26 @@
-import { Component, signal } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NetworkService } from './core/services/network.service';
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatSnackBarModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App {
+export class App implements OnInit {
   protected readonly title = signal('e-logbook');
+
+  constructor(private networkService: NetworkService, private snackBar: MatSnackBar) { }
+
+  ngOnInit() {
+    this.networkService.isOnline$.subscribe(isOnline => {
+      if (isOnline) {
+        this.snackBar.open('Connexion rétablie. Synchronisation en cours...', 'OK', { duration: 3000 });
+      } else {
+        this.snackBar.open('Mode Hors-Ligne activé. Vos données seront sauvegardées localement.', 'OK', { duration: 5000 });
+      }
+    });
+  }
 }
