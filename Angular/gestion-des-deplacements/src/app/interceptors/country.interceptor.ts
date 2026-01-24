60f4474ec4a68e109ae9d4a0bcb10ab519417451
Jonathan <jonathan.genet@gmail.com>
2026-01-24 16:15:49 +0100

    Fix: Bypass country interceptor for OSRM to prevent CORS errors

--- Angular/gestion-des-deplacements/src/app/interceptors/country.interceptor.ts
+++ Angular/gestion-des-deplacements/src/app/interceptors/country.interceptor.ts
@@ -1,9 +1,14 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 
 export const countryInterceptor: HttpInterceptorFn = (req, next) => {
+    // Skip external APIs (OSRM, etc.)
+    if (req.url.includes('router.project-osrm.org')) {
+        return next(req);
+    }
+
     // Get selected country from localStorage
     const selectedCountry = localStorage.getItem('selectedCountry');
-    console.log('[CountryInterceptor] Selected country:', selectedCountry);
+    // console.log('[CountryInterceptor] Selected country:', selectedCountry);
 
     // Add X-Selected-Country header if country is selected
     if (selectedCountry) {
@@ -12,10 +17,10 @@ export const countryInterceptor: HttpInterceptorFn = (req, next) => {
                 'X-Selected-Country': selectedCountry
             }
         });
-        console.log('[CountryInterceptor] Added header X-Selected-Country:', selectedCountry);
+        // console.log('[CountryInterceptor] Added header X-Selected-Country:', selectedCountry);
         return next(clonedReq);
     }
 
-    console.log('[CountryInterceptor] No country selected, no header added');
+    // console.log('[CountryInterceptor] No country selected, no header added');
     return next(req);
 };
