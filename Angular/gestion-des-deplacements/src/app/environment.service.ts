ecf2f675358b90e045bdedc02eb84ae781ac6446
Jonathan <jonathan.genet@gmail.com>
2026-01-26 11:16:43 +0100

    fix: remove broken environment import in service

--- Angular/gestion-des-deplacements/src/app/environment.service.ts
+++ Angular/gestion-des-deplacements/src/app/environment.service.ts
@@ -1,13 +1,11 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { environment } from '../../environments/environment';
-
 @Injectable({
     providedIn: 'root'
 })
 export class EnvironmentService {
-    private apiUrl = `${environment.apiUrl}/environment`;
+    private apiUrl = 'https://fleettrack-api.onrender.com/api/environment';
 
     constructor(private http: HttpClient) { }
 
