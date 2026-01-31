0b5466577b010968620c61356b08500031b1510d
Jonathan <jonathan.genet@gmail.com>
2026-01-31 14:35:41 +0100

    chore(backend): trigger redeploy to pick up new client build

--- Angular/backend/index.js
+++ Angular/backend/index.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+// Triggering backend redeploy for predictive maintenance fix
 const express = require('express');
 const path = require('path');
 const cors = require('cors');
